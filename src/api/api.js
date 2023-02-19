7760263dd8ba742de4d06fb8db303e9545fb0881
luqi-1998 <1277125008@qq.com>
2023-02-19 14:18:13 +0800

    cimit
    Signed-off-by: luqi-1998 <1277125008@qq.com>

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+import HTTP from "../utils/fetch";
+
+export const postUploadFile = () => {
+  return HTTP.get("./api/ss");
+};
+export const mergeUploadFile = () => {
+  return HTTP.get("./api/ss");
+};
+export const verifyUpload = () => {
+  return HTTP.get("./api/ss");
+};
